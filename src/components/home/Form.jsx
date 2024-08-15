40ba98df339073d18c7882cb12936bec443f1645
mohit <mohit21339@iiitd.ac.in>
2024-08-15 17:44:25 +0530

    updated form for grade

--- src/components/home/Form.jsx
+++ src/components/home/Form.jsx
@@ -235,9 +235,6 @@ function ReviewForm() {
                             { value: '6', label: '6' },
                             { value: '5', label: '5' },
                             { value: '4', label: '4' },
-                            { value: '3', label: '3' },
-                            { value: '2', label: '2' },
-                            { value: '1', label: '1' },
                         ]}
                         value={formData.grade}
                         onChange={(selectedOption) => handleInputChange('grade', selectedOption)}
